03d7e9259e0ccebac76194d91a6446e327f45712
Nolen Johnson <johnsonnolen@gmail.com>
2023-07-09 15:34:00 -0400

    quicksilver: Create aml_install phony target and tailor for us
    It's just a super slim package, containing a bootloader that
    forces booting into recovery mode and a super_empty image.
    
    Quicker, more reliable flashing process, plus solves the issue of
    entering recovery without buttons.
    
    Change-Id: I04fb916098006f9ac82ddaf379fdc1b357eef7c0

--- factory.mk
+++ factory.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2024 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,50 +14,91 @@
 # limitations under the License.
 #
 
-FACTORY_PATH := device/amlogic/quicksilver/factory
+FACTORY_PATH := device/xibo/quicksilver/factory
 
-PRODUCT_UPGRADE_OUT := $(PRODUCT_OUT)/upgrade
-PACKAGE_CONFIG_FILE := $(PRODUCT_UPGRADE_OUT)/image.cfg
+PRODUCT_INSTALL_OUT := $(PRODUCT_OUT)/aml_install
+PRODUCT_UPGRADE_OUT := $(PRODUCT_OUT)/aml_upgrade
+INSTALL_PACKAGE_CONFIG_FILE := $(PRODUCT_INSTALL_OUT)/image_install.cfg
+UPGRADE_PACKAGE_CONFIG_FILE := $(PRODUCT_UPGRADE_OUT)/image_upgrade.cfg
 AML_IMAGE_TOOL := $(HOST_OUT_EXECUTABLES)/aml_image_packer$(HOST_EXECUTABLE_SUFFIX)
 
+INSTALLED_AML_INSTALL_PACKAGE_TARGET := $(PRODUCT_OUT)/aml_install_package.img
 INSTALLED_AML_UPGRADE_PACKAGE_TARGET := $(PRODUCT_OUT)/aml_upgrade_package.img
 
-define aml-symlink-file
-	$(hide) ln -sf $(shell readlink -f $(1)) $(PRODUCT_UPGRADE_OUT)/$(strip $(if $(2), $(2), $(notdir $(1))))
+define aml-copy-install-file
+	$(hide) $(ACP) $(1) $(PRODUCT_INSTALL_OUT)/$(strip $(if $(2), $(2), $(notdir $(1))))
 endef
 
-NEEDED_IMAGES := \
+define aml-copy-upgrade-file
+	$(hide) $(ACP) $(1) $(PRODUCT_UPGRADE_OUT)/$(strip $(if $(2), $(2), $(notdir $(1))))
+endef
+
+UPGRADE_IMAGES := \
     boot.img \
     recovery.img \
     dtbo.img \
     vbmeta.img \
     vendor.img \
+    odm.img \
     system.img \
     logo.img
 
-$(INSTALLED_AML_UPGRADE_PACKAGE_TARGET): $(addprefix $(PRODUCT_OUT)/,$(NEEDED_IMAGES)) $(AML_IMAGE_TOOL)
-	$(hide) mkdir -p $(PRODUCT_UPGRADE_OUT)
-ifeq ("$(wildcard $(FACTORY_PATH)/u-boot.bin)","")
-	$(error "no u-boot.bin found in $(FACTORY_PATH)")
+INSTALL_IMAGES := \
+    boot.img \
+    recovery.img \
+    dtbo.img \
+    vbmeta.img \
+    logo.img \
+    misc.img
+
+$(INSTALLED_AML_INSTALL_PACKAGE_TARGET): $(addprefix $(PRODUCT_OUT)/,$(INSTALL_IMAGES)) $(ACP) $(AML_IMAGE_TOOL)
+	$(hide) mkdir -p $(PRODUCT_INSTALL_OUT)
+ifneq ("$(wildcard $(FACTORY_PATH)/u-boot.bin)","")
+	$(hide) $(call aml-copy-install-file, $(FACTORY_PATH)/u-boot.bin)
+else ifneq ("$(wildcard vendor/xibo/quicksilver/radio/bootloader.img)","")
+	$(hide) $(call aml-copy-install-file, vendor/xibo/quicksilver/radio/bootloader.img, u-boot.bin)
 else
-	$(hide) $(call aml-symlink-file, $(FACTORY_PATH)/u-boot.bin)
+	$(error "no u-boot.bin found in $(FACTORY_PATH)")
 endif
-	$(hide) $(call aml-symlink-file, $(FACTORY_PATH)/u-boot.bin.sd.bin)
-	$(hide) $(call aml-symlink-file, $(FACTORY_PATH)/u-boot.bin.usb.bl2)
-	$(hide) $(call aml-symlink-file, $(FACTORY_PATH)/u-boot.bin.usb.tpl)
-	$(hide) $(call aml-symlink-file, $(PRODUCT_OUT)/logo.img)
-	$(hide) $(call aml-symlink-file, $(FACTORY_PATH)/aml_sdc_burn.ini)
-	$(hide) $(call aml-symlink-file, $(FACTORY_PATH)/image.cfg)
-	$(hide) $(call aml-symlink-file, $(FACTORY_PATH)/platform.conf)
-	$(hide) $(call aml-symlink-file, $(PRODUCT_OUT)/boot.img)
-	$(hide) $(call aml-symlink-file, $(PRODUCT_OUT)/recovery.img)
-	$(hide) $(call aml-symlink-file, $(INSTALLED_2NDBOOTLOADER_TARGET), dtb.img)
-	$(hide) $(call aml-symlink-file, $(PRODUCT_OUT)/dtbo.img)
-	$(hide) $(call aml-symlink-file, $(PRODUCT_OUT)/vendor.img)
-	$(hide) $(call aml-symlink-file, $(PRODUCT_OUT)/odm.img)
-	$(hide) $(call aml-symlink-file, $(PRODUCT_OUT)/system.img)
-	$(hide) $(call aml-symlink-file, $(PRODUCT_OUT)/vbmeta.img)
-	$(hide) $(AML_IMAGE_TOOL) -r $(PACKAGE_CONFIG_FILE) $(PRODUCT_UPGRADE_OUT)/ $@
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/logo.img)
+	$(hide) $(call aml-copy-install-file, $(FACTORY_PATH)/aml_sdc_burn.ini)
+	$(hide) $(call aml-copy-install-file, $(FACTORY_PATH)/image_install.cfg, image.cfg)
+	$(hide) $(call aml-copy-install-file, $(FACTORY_PATH)/platform.conf)
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/boot.img)
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/recovery.img)
+	$(hide) $(call aml-copy-install-file, $(INSTALLED_2NDBOOTLOADER_TARGET), dtb.img)
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/dtbo.img)
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/vbmeta.img)
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/misc.img)
+	$(hide) $(AML_IMAGE_TOOL) -r  $(PRODUCT_INSTALL_OUT)/image.cfg $(PRODUCT_INSTALL_OUT)/ $@
+	$(hide) rm -rf $(PRODUCT_INSTALL_OUT)
+	$(hide) echo " $@ created"
+
+.PHONY: aml_install
+aml_install: $(INSTALLED_AML_INSTALL_PACKAGE_TARGET)
+
+$(INSTALLED_AML_UPGRADE_PACKAGE_TARGET): $(addprefix $(PRODUCT_OUT)/,$(UPGRADE_IMAGES)) $(ACP) $(AML_IMAGE_TOOL)
+	$(hide) mkdir -p $(PRODUCT_UPGRADE_OUT)
+ifneq ("$(wildcard $(FACTORY_PATH)/u-boot.bin)","")
+	$(hide) $(call aml-copy-upgrade-file, $(FACTORY_PATH)/u-boot.bin)
+else ifneq ("$(wildcard vendor/xibo/quicksilver/radio/bootloader.img)","")
+	$(hide) $(call aml-copy-upgrade-file, vendor/xibo/quicksilver/radio/bootloader.img, u-boot.bin)
+else
+	$(error "no u-boot.bin found in $(FACTORY_PATH)")
+endif
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/logo.img)
+	$(hide) $(call aml-copy-upgrade-file, $(FACTORY_PATH)/aml_sdc_burn.ini)
+	$(hide) $(call aml-copy-upgrade-file, $(FACTORY_PATH)/image_upgrade.cfg, image.cfg)
+	$(hide) $(call aml-copy-upgrade-file, $(FACTORY_PATH)/platform.conf)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/boot.img)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/recovery.img)
+	$(hide) $(call aml-copy-upgrade-file, $(INSTALLED_2NDBOOTLOADER_TARGET), dtb.img)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/dtbo.img)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/odm.img)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/system.img)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/vendor.img)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/vbmeta.img)
+	$(hide) $(AML_IMAGE_TOOL) -r  $(PRODUCT_UPGRADE_OUT)/image.cfg $(PRODUCT_UPGRADE_OUT)/ $@
 	$(hide) rm -rf $(PRODUCT_UPGRADE_OUT)
 	$(hide) echo " $@ created"
 
