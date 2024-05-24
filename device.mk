71a7a1e34f1df5601076acc55f8abf29461c6c32
Francesco "Dil3mm4" Manzo <dil3mm4.dev@gmail.com>
2024-05-24 15:48:51 +0000

    quicksilver: include own media_codecs_performance and media_profiles
    Change-Id: If662ff2ed793fb41659b0d3fd5b966eedbcb670f

--- device.mk
+++ device.mk
@@ -23,9 +23,15 @@ TARGET_DHD_VERSION := bcmdhd.100.10.545.x
 PRODUCT_PACKAGES += \
     dhd
 
-## Media
+## Codecs
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
+    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0
+
+ifneq ($(PRODUCT_USE_SW_OMX),true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
+endif
 
 # Platform
 TARGET_AMLOGIC_SOC := g12b
